5824678b20979b4f41e9c79e8312eddc8b57508d
irene <irene.farellano@alumnos.upm.es>
2025-10-24 15:22:46 +0200

    InitShot mejora

--- Entrega1CajaNegra/src/test/java/es/upm/iwt41/entrega1/PruebasShot/ShotTest.java
+++ Entrega1CajaNegra/src/test/java/es/upm/iwt41/entrega1/PruebasShot/ShotTest.java
@@ -10,39 +10,14 @@ public class ShotTest {
     private final int H_SPACE = 6;
     private final int V_SPACE = 1;
 
-    @Test
-    void initShotCorrecto()
+
+    @org.junit.jupiter.params.ParameterizedTest
+    @org.junit.jupiter.params.provider.CsvFileSource(resources= "/pruebasInitShot.csv",
+            numLinesToSkip= 1,
+            lineSeparator = "\n",
+            delimiterString = ",")
+    void initShotCorrecto(int x, int y)
     {
-        int x = Commons.BOARD_WIDTH/2;
-        int y = Commons.BOARD_HEIGHT - Commons.GROUND;
-
-        Shot shot = new Shot(x, y);
-
-        boolean resultado = shot.getX() == x+H_SPACE  && shot.getY() == y-V_SPACE;
-        assertTrue(resultado);
-    }
-
-
-    @Test
-    void initShotCorrectoFueraLimitesX()
-    {
-        int x = -1;
-        int y = 50;
-        int H_SPACE = 6;
-        int V_SPACE = 1;
-        Shot shot = new Shot(x, y);
-
-        boolean resultado = shot.getX() == x+H_SPACE  && shot.getY() == y-V_SPACE;
-        assertTrue(resultado);
-    }
-
-    @Test
-    void initShotCorrectoFueraLimitesY()
-    {
-        int x = 50;
-        int y = -1;
-        int H_SPACE = 6;
-        int V_SPACE = 1;
         Shot shot = new Shot(x, y);
 
         boolean resultado = shot.getX() == x+H_SPACE  && shot.getY() == y-V_SPACE;
