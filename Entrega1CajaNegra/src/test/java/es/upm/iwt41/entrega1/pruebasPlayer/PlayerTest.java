6ab1e82a459eb5e7c04b9eb6478a8089aa5882e6
danie <daniel.galan.santos@gmail.com>
2025-10-29 20:26:17 +0100

    PlayerTest-KeyPressed firstTry(irene aceptamelo porfis)

--- Entrega1CajaNegra/src/test/java/es/upm/iwt41/entrega1/pruebasPlayer/PlayerTest.java
+++ Entrega1CajaNegra/src/test/java/es/upm/iwt41/entrega1/pruebasPlayer/PlayerTest.java
@@ -1,8 +1,11 @@
 package es.upm.iwt41.entrega1.pruebasPlayer;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import space_invaders.sprites.Player;
+import java.awt.TextField;
+import java.awt.event.KeyEvent;
 
 public class PlayerTest
 {
@@ -21,4 +24,52 @@ public class PlayerTest
         int desplazamientoAnterior = player.get;
 
     }
+
+
+
+    @Test
+    public void testKeyPressed() {
+        // Instancia del objeto a testear
+
+        // Creación del evento que simula pulsar la flecha derecha
+        KeyEvent evento = new KeyEvent(
+                new TextField(),              // componente origen del evento
+                KeyEvent.KEY_PRESSED,         // tipo
+                System.currentTimeMillis(),   // timestamp
+                0,                            // modificadores
+                KeyEvent.VK_RIGHT,            // código de la tecla
+                KeyEvent.CHAR_UNDEFINED
+        );
+
+        // Afirmación
+        assertEquals(2, player.getdx;
+
+
+        // Creación del evento que simula pulsar la flecha izquierda
+         evento = new KeyEvent(
+                new TextField(),              // componente origen del evento
+                KeyEvent.KEY_PRESSED,         // tipo
+                System.currentTimeMillis(),   // timestamp
+                0,                            // modificadores
+                KeyEvent.VK_LEFT,            // código de la tecla
+                KeyEvent.CHAR_UNDEFINED
+        );
+
+        // Afirmación
+        assertEquals(-2, player.keyPressed(evento));
+
+        // Creación del evento que simula pulsar la flecha izquierda
+        evento = new KeyEvent(
+                new TextField(),              // componente origen del evento
+                KeyEvent.KEY_PRESSED,         // tipo
+                System.currentTimeMillis(),   // timestamp
+                0,                            // modificadores
+                KeyEvent.VK_UP,            // código de la tecla
+                KeyEvent.CHAR_UNDEFINED
+        );
+
+        // Afirmación
+        assertEquals(0, player.keyPressed(evento));
+    }
 }
+
