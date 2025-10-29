2da6d4d38849540120a17a18ada664c3ab39bb3b
irene <irene.farellano@alumnos.upm.es>
2025-10-29 20:27:08 +0100

    cambio PlayerTest

--- Entrega1CajaNegra/src/test/java/es/upm/iwt41/entrega1/pruebasPlayer/PlayerTest.java
+++ Entrega1CajaNegra/src/test/java/es/upm/iwt41/entrega1/pruebasPlayer/PlayerTest.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import space_invaders.sprites.Player;
 
+import java.awt.event.KeyEvent;
+import java.awt.TextField;
+import java.awt.event.KeyEvent;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class PlayerTest
 {
     Player player;
@@ -12,13 +19,25 @@ public class PlayerTest
         this.player = new Player();
     }
 
-    @org.junit.jupiter.params.ParameterizedTest
-    @org.junit.jupiter.params.provider.CsvSource(value = {
-            "37",
-            "39",
-            "38"})
-    void keyReleased(int e){
-        int desplazamientoAnterior = player.get;
+   @Test
+    void keyReleasedRight(){
+        KeyEvent keyEvent = new KeyEvent(new TextField(), KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
+        player.keyReleased(keyEvent);
+        assertEquals(0, player.getDx());
+    }
 
+    @Test
+    void keyReleasedLeft(){
+        KeyEvent keyEvent = new KeyEvent(new TextField(), KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
+        player.keyReleased(keyEvent);
+        assertEquals(0, player.getDx());
+    }
+
+    @Test
+    void keyReleasedUp(){
+        KeyEvent keyEvent = new KeyEvent(new TextField(), KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, KeyEvent.VK_UP, KeyEvent.CHAR_UNDEFINED);
+        int dx_anterior = player.getDx();
+        player.keyReleased(keyEvent);
+        assertEquals(dx_anterior, player.getDx());
     }
 }
