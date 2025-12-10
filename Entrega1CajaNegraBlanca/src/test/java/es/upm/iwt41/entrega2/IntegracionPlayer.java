7d132ad866674613aea66192accffcbc31ec7215
zeth <myzhe@alu.upo.es>
2025-12-10 09:17:16 +0100

    Depuración y Main.java

--- Entrega1CajaNegraBlanca/src/test/java/es/upm/iwt41/entrega2/IntegracionPlayer.java
+++ Entrega1CajaNegraBlanca/src/test/java/es/upm/iwt41/entrega2/IntegracionPlayer.java
@@ -1,4 +1,22 @@
 package es.upm.iwt41.entrega2;
 
+import main.Commons;
+import org.junit.jupiter.api.Test;
+import space_invaders.sprites.Player;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntegracionPlayer {
+
+    @Test
+    void testInvocaInitPlayer() {
+
+        Player player = new Player();
+
+        //al llamar al constructor de player, se crea un objeto player con las posiciones x e y especificadas
+
+        assertEquals(Commons.BOARD_WIDTH / 2,player.getX(),"El jugador debe estar centrado en el tablero");
+        assertEquals(Commons.GROUND - 10,player.getY(),"El jugador debe estar 10px por encima del ground");
+
+    }
 }
+
--- Entrega1CajaNegraBlanca/src/test/java/es/upm/iwt41/entrega2/IntegracionShot.java
+++ Entrega1CajaNegraBlanca/src/test/java/es/upm/iwt41/entrega2/IntegracionShot.java
@@ -1,4 +1,24 @@
 package es.upm.iwt41.entrega2;
 
+import org.junit.jupiter.api.Test;
+import space_invaders.sprites.Shot;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntegracionShot {
+
+    @Test
+    void testInvocaInitShot() {
+
+        int xEntrada = 100;
+        int yEntrada = 50;
+        int xEsperado = xEntrada + 6;
+        int yEsperado = yEntrada - 1;
+
+        Shot shot = new Shot(xEntrada, yEntrada);
+
+        assertEquals(xEsperado,shot.getX(),"La X del disparo debe sumar H_SPACE (6) a la entrada");
+
+        assertEquals(yEsperado,shot.getY(),"La Y del disparo debe restar V_SPACE (1) a la entrada");
+
+    }
 }
